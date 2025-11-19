404280f39739871bd28dfc35fd2b273f3430a9f5
Arjun Maneesh Agarwal <149602712+TheArjunAgarwal@users.noreply.github.com>
2025-11-19 07:23:44 +0000

    populate the list

--- src/data/books.ts
+++ src/data/books.ts
@@ -19,36 +19,131 @@ export const mainBooks: Book[] = [
     coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1751820530i/29044.jpg",
     rating: 4.15,
     pageCount: 559,
-    difficulty: "Easy", 
-    // Easy Moderate Advanced Challenging
+    difficulty: "Easy",
     pitch: "",
     whyOnList: ""
   },
-// Template
-  // {
-  //   id: "",
-  //   title: "",
-  //   author: "",
-  //   coverImage: "",
-  //   rating: ,
-  //   pageCount: ,
-  //   difficulty: "", 
-  //   // Easy Moderate Advanced Challenging
-  //   pitch: "",
-  //   whyOnList: ""
-  // },
-
+  {
+    id: "3",
+    title: "Tomorrow, and Tomorrow, and Tomorrow",
+    author: "Gabrielle Zevin",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1636741071i/58784475.jpg",
+    rating: 4.32,
+    pageCount: 416,
+    difficulty: "Easy",
+    pitch: "",
+    whyOnList: ""
+  },  
+  {
+    id: "4",
+    title: "The Book of Disquiet",
+    author: "Fernando Pessoa",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1349004790i/30133.jpg",
+    rating: 4.19,
+    pageCount: 544,
+    difficulty: "Challenging",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "5",
+    title: "Unflattening",
+    author: "Nick Sousanis",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1427134899i/22521931.jpg",
+    rating: 4.19,
+    pageCount: 208,
+    difficulty: "Moderate",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "6",
+    title: "Vineland",
+    author: "Thomas Pynchon",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1335762033i/4894.jpg",
+    rating: 3.77,
+    pageCount: 385,
+    difficulty: "Challenging",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "7",
+    title: "A Breath of Life",
+    author: "Clarice Lispector (trans. Johnny Lorenz)",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1330784381i/12512001.jpg",
+    rating: 4.18,
+    pageCount: 208,
+    difficulty: "Challenging",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "8",
+    title: "Talking to My Daughter About the Economy",
+    author: "Yanis Varoufakis",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1508135687i/36348193.jpg",
+    rating: 4.05,
+    pageCount: 224,
+    difficulty: "Moderate",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "9",
+    title: "A Psalm for the Wild-Built",
+    author: "Becky Chambers",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1610707376i/40864002.jpg",
+    rating: 4.33,
+    pageCount: 152,
+    difficulty: "Easy",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "10",
+    title: "The Left Hand of Darkness",
+    author: "Ursula K. Le Guin",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1388213255i/18423.jpg",
+    rating: 4.11,
+    pageCount: 304,
+    difficulty: "Moderate",
+    pitch: "",
+    whyOnList: ""
+  }
 ];
 
 export const honorableMentions: Book[] = [
   {
-    id: "7",
+    id: "11",
     title: "Honor",
     author: "Thrity Umrigar",
     coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1640084756i/56271394.jpg",
     rating: 4.35,
     pageCount: 326,
     difficulty: "Moderate",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "12",
+    title: "Stone Butch Blues",
+    author: "Leslie Feinberg",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1348747946i/669320.jpg",
+    rating: 4.60,
+    pageCount: 301,
+    difficulty: "Moderate",
+    pitch: "",
+    whyOnList: ""
+  },
+  {
+    id: "13",
+    title: "The Gods Will Have Blood",
+    author: "Anatole France",
+    coverImage: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1426949130i/348531.jpg",
+    rating: 3.81,
+    pageCount: 368,
+    difficulty: "Advanced",
     pitch: "",
     whyOnList: ""
   }
