696c79d3a044e3a287163cd5f6e211d85c8de4bd
TheArjunAgarwal <anulick0@gmail.com>
2025-12-03 05:18:08 +0530

    filled stuff

--- src/data/books.ts
+++ src/data/books.ts
@@ -9,8 +9,8 @@ export const mainBooks: Book[] = [
     rating: 4.52,
     pageCount: 331,
     difficulty: "Challenging",
-    pitch: "",
-    whyOnList: ""
+    pitch: "Dark, sad and emotionally draining. Arundhati Roy's deeply intellectual style combined with a personal narrative. \nWe are used to seeing the world in black and white, we want easy answers, easy solutions, easy categorizations, quick labels for people. In real life, it’s not always that simple.",
+    whyOnList: "Because the anonymous suggester said they really wanted to read this and wanted motivation. Also, we had heard good things about the book.\nThe story is about the complex and multi-layered relationship of two remarkable but flawed women.\nThough not as well-known as her daughter, Mary Roy was too a remarkable woman: a formidable feminist and an inspiring educator who transformed many lives in Kerala. However, she too was not without her flaws. Mother Mary Comes to Me reveals the fullness, trouble and tumult, of her legacy through the eyes of her 'valiant organ-child'. As it follows the separate but interminably linked journeys of the two women, Mary and Arundhati, it shapes up into an unflinching, honest portrait of the complexity and irreducibility of human relationships – of love."
   },
   {
     id: "2",
@@ -20,8 +20,8 @@ export const mainBooks: Book[] = [
     rating: 4.15,
     pageCount: 559,
     difficulty: "Easy",
-    pitch: "",
-    whyOnList: ""
+    pitch: "The quintessential dark academia book. Not only did it pioneer a genre, it basically gave birth to an aesthetic. I'll let the blurb do rest of the talking:\nUnder the influence of their charismatic classics professor, a group of clever, eccentric misfits at an elite New England college discover a way of thinking and living that is a world away from the humdrum existence of their contemporaries. But when they go beyond the boundaries of normal morality they slip gradually from obsession to corruption and betrayal, and at last inexorably—into evil.",
+    whyOnList: "It is a modern classic and has one of the best blurbs of like all time. Also, it is a fun and easy read."
   },
   {
     id: "3",
@@ -31,8 +31,8 @@ export const mainBooks: Book[] = [
     rating: 4.12,
     pageCount: 416,
     difficulty: "Easy",
-    pitch: "",
-    whyOnList: ""
+    pitch: "The person who suggested this said: \"It's a book about friendship, love and more friendship. and games! It's a perfect read for the vacations\" and we couldn't agree more. It was the 2022 Goodreads choice award winner (Fiction).",
+    whyOnList: "From the book's blurb:\nSpanning thirty years, from Cambridge, Massachusetts, to Venice Beach, California, and lands in between and far beyond, Gabrielle Zevin's Tomorrow, and Tomorrow, and Tomorrow is a dazzling and intricately imagined novel that examines the multifarious nature of identity, disability, failure, the redemptive possibilities in play, and above all, our need to connect: to be loved and to love. Yes, it is a love story, but it is not one you have read before."
   },  
   {
     id: "4",
@@ -42,8 +42,8 @@ export const mainBooks: Book[] = [
     rating: 4.10,
     pageCount: 544,
     difficulty: "Challenging",
-    pitch: "",
-    whyOnList: ""
+    pitch: "The book has a nickname: \"Factless Autobiography\". It is about the many personas of Fernando Pessoa, each with a different world view and style.",
+    whyOnList: "We felt the premise and the history (and significance to Portuguese literature) warranted it being placed on the list. It is a nice experimental and challenging read."
   },
   {
     id: "5",
@@ -53,8 +53,8 @@ export const mainBooks: Book[] = [
     rating: 4.07,
     pageCount: 208,
     difficulty: "Moderate",
-    pitch: "",
-    whyOnList: ""
+    pitch: "We'll let the wikipedia sell you on this one:\nUnflattening is a graphic novel by artist and researcher Nick Sousanis that was originally the first dissertation from Columbia University to be written in a comic book format.",
+    whyOnList: "Given the theme this list has gone for, this seemed like a fitting graphic novel to keep on the spot. It also is an extremely interesting and approachable dissertation, something I want to see more of."
   },
   {
     id: "6",
@@ -86,8 +86,8 @@ export const mainBooks: Book[] = [
     rating: 4.13,
     pageCount: 224,
     difficulty: "Moderate",
-    pitch: "",
-    whyOnList: ""
+    pitch: "The former finance minister of Greece explains, in plain english, global finance and economics to his daughter. Helps one understand what currency rates, tariffs, trade sanctions etc are and what they really mean.",
+    whyOnList: "1. We wanted to have a book for people who don't want to read fiction.\n2. The world would honestly be better if people knew macro-economics.\n3. It is genuinely filled with curiosity, humility and charm."
   },
   {
     id: "9",
@@ -97,7 +97,7 @@ export const mainBooks: Book[] = [
     rating: 4.22,
     pageCount: 151,
     difficulty: "Easy",
-    pitch: "",
+    pitch: "With AI fear all around, this book imagines a different future. Why AI and humans can co-exist and make life better. The book is about the conversations between a robot and a tea-monk about \"what do people need?\". An easy, uplifting and hopeful read.",
     whyOnList: ""
   },
   {
@@ -122,8 +122,8 @@ export const honorableMentions: Book[] = [
     rating: 4.35,
     pageCount: 326,
     difficulty: "Moderate",
-    pitch: "",
-    whyOnList: ""
+    pitch: "A fiction exploring honor killings in India and it's effect on society. Written by an Indian and forces the reader to ask the hard questions.",
+    whyOnList: "The only reason it is not on the list is that it is quite a sad and depressing story about the female experience in India. We felt Arundhiti Roy already covered it."
   },
   {
     id: "12",
